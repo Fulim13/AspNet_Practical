c279cabd8e8f560263d8555e7a3061ea8a7bd142
Fu Lim <fulim1130@gmail.com>
2024-02-27 16:13:55 +0800

    Practical 1 - Server Control object's method and event

--- Practical1/Practical1/WebForm1.aspx.cs
+++ Practical1/Practical1/WebForm1.aspx.cs
@@ -11,7 +11,12 @@ namespace Practical1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            btnSubmit.Focus();
+        }
 
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "Hello " + txtName.Text + "!";
         }
     }
 }
