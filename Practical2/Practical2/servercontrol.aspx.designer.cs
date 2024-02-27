1b7f85566b76ca199bbe1544478f8c943b5a1920
Fu Lim <fulim1130@gmail.com>
2024-02-27 16:40:33 +0800

    Practical 2 - Web Forms (with) sharing Master Page

--- Practical2/Practical2/servercontrol.aspx.designer.cs
+++ Practical2/Practical2/servercontrol.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace Practical2
     {
 
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
         /// txtName control.
         /// </summary>
         /// <remarks>
@@ -58,14 +49,5 @@ namespace Practical2
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSubmit;
-
-        /// <summary>
-        /// lblSelect control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblSelect;
     }
 }
