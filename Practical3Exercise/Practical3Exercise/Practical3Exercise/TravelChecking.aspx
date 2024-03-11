56e156f26836e23d9597a9a4faebbac4bce3a7c8
Fu Lim <fulim1130@gmail.com>
2024-03-12 00:45:25 +0800

    Practical 3 Exercise 1 Partially Done

---
+++ Practical3Exercise/Practical3Exercise/Practical3Exercise/TravelChecking.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TravelChecking.aspx.cs" Inherits="Practical3Exercise.TravelChecking" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 170px;
+        }
+        .auto-style2 {
+            width: 232px;
+        }
+        .auto-style3 {
+            width: 29%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Holiday Travel Package<br />
+            <br />
+            <asp:Label ID="lblWelcomeMessage" runat="server" Text="[lblWelcomeMessage]"></asp:Label>
+            <br />
+            Your Budget:<asp:TextBox ID="tbxBudget" runat="server" TextMode="Number"></asp:TextBox>
+            <br />
+            <br />
+            Choose your destination:<br />
+            <br />
+            <table class="auto-style3">
+                <tr>
+                    <td class="auto-style1">From</td>
+                    <td class="auto-style2">To</td>
+                </tr>
+                <tr>
+                    <td class="auto-style1">
+                        <asp:DropDownList ID="ddlFrom" runat="server">
+                            <asp:ListItem>KL</asp:ListItem>
+                            <asp:ListItem>Penang</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                    <td class="auto-style2">
+                        <asp:DropDownList ID="ddlTo" runat="server">
+                            <asp:ListItem>Japan</asp:ListItem>
+                            <asp:ListItem>Auckland</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <p>
+            <asp:Button ID="btnCheck" runat="server" OnClick="btnCheck_Click" Text="Check" />
+        </p>
+        <p>
+            Your destination : <asp:Label ID="lblMessage" runat="server" Text="[lblMessage]"></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ Practical3Exercise/Practical3Exercise/Practical3Exercise/TravelChecking.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.CompensatingResourceManager;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical3Exercise
+{
+    public partial class TravelChecking : System.Web.UI.Page
+    {
+        double fromKLToJapan = 2400;
+        double fromKLToAuckland = 2200;
+        double fromPenangToJapan = 3400;
+        double fromPenangToAuckland = 3200;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            lblWelcomeMessage.Text = "Happy Holiday from MOM Travel";
+
+            if (!IsPostBack) 
+            {
+
+                lblWelcomeMessage.Text = "Thank you for using our service";
+                
+            }
+        }
+
+        private double calculatePrice(string from ,string to)
+        {
+            double price = 0;
+            
+            return price;
+        }
+
+        //a function differencesPrice to return the price differences between the destination price and your budgeted value.
+        private double differencesPrice(double price, double budget)
+        {
+           
+            return price - budget;
+        }
+
+        protected void btnCheck_Click(object sender, EventArgs e)
+        {
+            if(ddlFrom.SelectedItem.Text == "Kuala Lumpur" && ddlTo.SelectedItem.Text == "Japan")
+            {
+                if(tbxBudget.Text != "")
+                {
+                    double budget = Convert.ToDouble(tbxBudget.Text);
+                    double price = fromKLToJapan;
+                    double differences = differencesPrice(price, budget);
+                    if(differences > 0)
+                    {
+                        lblMessage.Text = "The price is RM" + price + " and the differences is RM" + differences;
+                    }
+                    else
+                    {
+                        lblPrice.Text = "The price is RM" + price + " and the differences is RM" + differences;
+                    }
+                }
+ 
+            }
+            else if (ddlFrom.SelectedItem.Text == "Kuala Lumpur" && ddlTo.SelectedItem.Text == "Auckland")
+            {
+                lblPrice.Text = "The price is RM" + fromKLToAuckland;
+            }
+            else if (ddlFrom.SelectedItem.Text == "Penang" && ddlTo.SelectedItem.Text == "Japan")
+            {
+                lblPrice.Text = "The price is RM" + fromPenangToJapan;
+            }
+            else if (ddlFrom.SelectedItem.Text == "Penang" && ddlTo.SelectedItem.Text == "Auckland")
+            {
+                lblPrice.Text = "The price is RM" + fromPenangToAuckland;
+            }
+            else
+            {
+                lblPrice.Text = "The price is RM0";
+            }
+        }
+    }
+}
