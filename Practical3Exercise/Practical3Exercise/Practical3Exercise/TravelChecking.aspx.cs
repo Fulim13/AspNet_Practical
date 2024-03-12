ac0cc5da4d9a388703760e99c8c2c16398aa653b
Fu Lim <fulim1130@gmail.com>
2024-03-12 17:00:00 +0800

    Complete Practical 3 Exercise 1

--- Practical3Exercise/Practical3Exercise/Practical3Exercise/TravelChecking.aspx.cs
+++ Practical3Exercise/Practical3Exercise/Practical3Exercise/TravelChecking.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.EnterpriseServices.CompensatingResourceManager;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,73 +13,67 @@ namespace Practical3Exercise
 {
     public partial class TravelChecking : System.Web.UI.Page
     {
-        double fromKLToJapan = 2400;
-        double fromKLToAuckland = 2200;
-        double fromPenangToJapan = 3400;
-        double fromPenangToAuckland = 3200;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
             lblWelcomeMessage.Text = "Happy Holiday from MOM Travel";
 
-            if (!IsPostBack) 
+            if (!IsPostBack)
             {
 
                 lblWelcomeMessage.Text = "Thank you for using our service";
-                
+
             }
         }
 
-        private double calculatePrice(string from ,string to)
+        private double calculatePrice(string from, string to)
         {
             double price = 0;
-            
+            if (from == "KL" && to == "Japan")
+            {
+                price = 2400;
+            }
+            else if (from == "KL" && to == "Auckland")
+            {
+                price = 2200;
+            }
+            else if (from == "Penang" && to == "Japan")
+            {
+                price = 3400;
+            }
+            else if (from == "Penang" && to == "Auckland")
+            {
+                price = 3200;
+            }
             return price;
         }
 
-        //a function differencesPrice to return the price differences between the destination price and your budgeted value.
-        private double differencesPrice(double price, double budget)
+        private double differencesPrice(double budget, double price)
         {
-           
-            return price - budget;
+            return budget - price;
         }
 
         protected void btnCheck_Click(object sender, EventArgs e)
         {
-            if(ddlFrom.SelectedItem.Text == "Kuala Lumpur" && ddlTo.SelectedItem.Text == "Japan")
+
+            if (tbxBudget.Text != "")
             {
-                if(tbxBudget.Text != "")
+                double budget = Convert.ToDouble(tbxBudget.Text);
+                double price = calculatePrice(ddlFrom.SelectedItem.Text, ddlTo.SelectedItem.Text);
+                double differences = differencesPrice(budget, price);
+                if (differences > 0)
                 {
-                    double budget = Convert.ToDouble(tbxBudget.Text);
-                    double price = fromKLToJapan;
-                    double differences = differencesPrice(price, budget);
-                    if(differences > 0)
-                    {
-                        lblMessage.Text = "The price is RM" + price + " and the differences is RM" + differences;
-                    }
-                    else
-                    {
-                        lblPrice.Text = "The price is RM" + price + " and the differences is RM" + differences;
-                    }
+                    lblMessage.Text = ddlFrom.SelectedItem.Text + "-" + ddlTo.SelectedItem.Text + "<br />" + "Yay! Can go for holiday!" + "<br />" + "RM " + differences + " extra pocket money!";
                 }
- 
+                else
+                {
+                    lblMessage.Text = ddlFrom.SelectedItem.Text + "-" + ddlTo.SelectedItem.Text + "<br />" + "Do not meet your budget" + "<br />" + "RM " + differences;
+
+                }
             }
-            else if (ddlFrom.SelectedItem.Text == "Kuala Lumpur" && ddlTo.SelectedItem.Text == "Auckland")
-            {
-                lblPrice.Text = "The price is RM" + fromKLToAuckland;
-            }
-            else if (ddlFrom.SelectedItem.Text == "Penang" && ddlTo.SelectedItem.Text == "Japan")
-            {
-                lblPrice.Text = "The price is RM" + fromPenangToJapan;
-            }
-            else if (ddlFrom.SelectedItem.Text == "Penang" && ddlTo.SelectedItem.Text == "Auckland")
-            {
-                lblPrice.Text = "The price is RM" + fromPenangToAuckland;
-            }
-            else
-            {
-                lblPrice.Text = "The price is RM0";
-            }
+
+
         }
     }
 }
